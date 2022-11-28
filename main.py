3a4bc151c6e7138b06a12f692cb86d19da32f774
VmTalik <vmarlyan@mail.ru>
2022-11-28 21:32:27 +0300

    Fix code, build architecture

--- main.py
+++ main.py
@@ -1,95 +1,75 @@
 import sqlite3 as sq
+from SQL_queries_data import position_table, employees_table, position_name_data, \
+    insert_into_position, employees_data, insert_into_employees, \
+    select_1, select_2, select_3, select_4, select_5
 
 
 def create_database(db: str):
     """Функция для создания базы данных отдела кадров"""
     with sq.connect(db) as con:
         cur = con.cursor()
-        cur.execute("""CREATE TABLE IF NOT EXISTS position (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            position_name TEXT
-            );
-        """)
-        cur.execute("""CREATE TABLE IF NOT EXISTS employees (
-            id INTEGER NOT NULL,
-            full_name TEXT,
-            sex TEXT,
-            birth_year INTEGER,
-            address TEXT,
-            job_start_year INTEGER,
-            experience AS (2017 - job_start_year),
-            promotion TEXT,
-            position_id INTEGER,
-            FOREIGN KEY (position_id) REFERENCES position (id),
-            CONSTRAINT personnel_number PRIMARY KEY(id)
-            );
-        """)
+        cur.execute(position_table)
+        cur.execute(employees_table)
 
 
 def populate_database(db: str):
     """Функция заполнения базы данных отдела кадров"""
     with sq.connect(db) as con:
         cur = con.cursor()
-        # запрос на внесение данных в таблицу position
-        sql = 'INSERT INTO position (position_name) values(?)'
-        # указываем данные для запроса
-        data = [
-            ('Аналитик',),
-            ('Программист',),
-            ('Менеджер проектов',),
-            ('Секретарь',),
-        ]
-        # добавляем записи в таблицу position
-        cur.executemany(sql, data)
-        # запрос на внесение данных в таблицу employees
-        sql = 'INSERT INTO employees (id, full_name, sex, birth_year, address, ' \
-              'job_start_year, promotion, position_id) values(?, ?, ?, ?, ?, ?, ?, ?)'
-        # указываем данные для запроса
-        data = [
-            (1000, 'Карпов В.И.', 'муж.', 1978, 'Капроновская наб. 1', 2004, 'есть', 3),
-            (1001, 'Варламов П.И.', 'муж.', 1988, ' Варгуновский бульвар, 2', 2007, 'нет', 2),
-            (1002, 'Карасева А.Г.', 'жен.', 1992, 'Батайская улица, 1', 2008, 'нет', 1),
-            (1003, 'Потапов И.Е.', 'муж.', 1970, 'Северный 2-й проезд, 3', 1999, 'есть', 2),
-            (1004, 'Вешнякова В.М.', 'жен.', 1990, 'Ореховый бульвар, 2', 2012, 'нет', 4),
-        ]
-        # добавляем записи в таблицу employees
-        cur.executemany(sql, data)
+        cur.executemany(insert_into_position, position_name_data)
+        cur.executemany(insert_into_employees, employees_data)
 
 
-def get_query_result(db: str, q: int):
-    """Функция получения результата требуемого запроса к базе данных
-    Функция запроса на получение данных из БД"""
+def get_query_result(db: str, q: int) -> any:
+    """Функция получения результата требуемого запроса к базе данных"""
     with sq.connect(db) as con:
         cur = con.cursor()
         if q == 1:
-            s = input('Введите первую букву фамилии сотрудника:\n')
-            pass
+            s = input('Введите первую букву фамилии сотрудника:\n').capitalize()
+            cur.execute(select_1.format(s))
+            rows = cur.fetchall()
+            if len(rows) != 0:
+                return rows
+            else:
+                return 'Фамилий сотрудников, начинающихся на введенную букву, нет в БД'
+
         elif q == 2:
-            pass
+            cur.execute(select_2)
+            rows = cur.fetchall()
+            return rows
         elif q == 3:
-            # выбор диапазона табельных номеров из базы данных
             while True:
                 try:
-                    min_number = int(input())
-                    max_number = int(input())
-                    break
+                    min_n, max_n = map(int, input('Введите диапазон табельных номеров через пробел:').split())
+                    cur.execute(select_3.format(min_n, max_n))
+                    rows = cur.fetchall()
+                    if len(rows) != 0:
+                        return rows
+                    else:
+                        return 'Сотрудников из данного диапазона табельных номеров в БД нет'
                 except ValueError:
-                    print('Ошибка ввода. Должно быть целое число!')
-
-            pass
+                    return 'Ошибка ввода. Должен быть диапазон целых чисел, введеннй через пробел'
         elif q == 4:
-            pass
-        else:
-            sex = input('Введите пол сотрудника:\n')
+            cur.execute(select_4)
+            rows = cur.fetchall()
+            if len(rows) != 0:
+                return rows
+            else:
+                return 'У сотрудников нет поощрений'
+
+        elif q == 5:
+            sex = input('Введите пол сотрудника (м/ж):\n').lower()
             while True:
                 try:
-                    birth_year = int('Введите год рождения сотрудника:\n')
-                    break
+                    birth_year = int(input('Введите год рождения сотрудника:\n'))
+                    cur.execute(select_5.format(birth_year, sex))
+                    rows = cur.fetchall()
+                    if len(rows) != 0:
+                        return rows
+                    else:
+                        return 'Таких сотрудников нет в БД'
                 except ValueError:
-                    print('Ошибка ввода. Должно быть целое число!')
-
-            pass
-        return 'результат'
+                    return 'Ошибка ввода. Должно быть целое число!'
 
 
 def choose_command() -> int:
@@ -97,10 +77,11 @@ def choose_command() -> int:
     task = """
     Программа по требованию выдает списки:
     1. По заданной первой букве фамилии сотрудника
-    2. По стажу
+    2. По убыванию стажа работы сотрудников
     3. По заданному пользователем диапазону табельных номеров,
-    отсортированный по году рождения
-    4. По поощрениям,отсортированный по стажу
+    сотрудники должны быть отсортированы по убыванию года рождения
+    4. По поощрениям, сотрудники должы быть отсортированы по
+     убыванию стажа работы
     5. По заданному пользователем году рождения и полу\n
     Выберете, какой список Вы хотите получить, введите число от 1 до 5:
     """
